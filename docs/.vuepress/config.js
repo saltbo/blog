36bbea7fbc325b9b414ea0d26fcf794df522e282
saltbo <saltbo@foxmail.com>
2020-04-11 22:13:13 +0800

    feat: force redirect to the https

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,11 @@ module.exports = {
   dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+    ['script', { type: 'text/javascript' }, `
+      var targetProtocol = "https:";
+      if (window.location.host != localhost && window.location.protocol != targetProtocol)
+        window.location.href = targetProtocol + window.location.href.substring(window.location.protocol.length);`]
   ],
   // theme: '@vuepress/theme-blog',
   themeConfig: {
