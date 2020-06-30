a437c792377a6e78daee977ad9600519472b27dd
saltbo <saltbo@foxmail.com>
2020-06-30 21:59:14 +0800

    fix: none article summary

--- docs/.vuepress/theme/layouts/Feeds.vue
+++ docs/.vuepress/theme/layouts/Feeds.vue
@@ -1,27 +1,20 @@
 <template>
   <div class="container">
     <div class="box with-padding">
-      <timeline>
-        <timeline-title>太棒了! 目前共计 {{articles.length}} 篇日志。 继续努力。</timeline-title>
-        <timeline-item v-for="article in articles">
+       <div v-for="article in articles">
           <h3 v-if="!article.id">{{ article.title }}</h3>
           <router-link v-else class="post-link" :to="article.path">
-            <span class="date">{{dateFormat(article.frontmatter.date)}}</span>
+            <span class="date">{{ dateFormat(article.frontmatter.date) }}</span>
             <span class="title">{{ article.title }}</span>
           </router-link>
-        </timeline-item>
-      </timeline>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Timeline, TimelineItem, TimelineTitle } from 'vue-cute-timeline'
 export default {
   components: {
-    Timeline,
-    TimelineItem,
-    TimelineTitle
   },
   methods:{
     compareDate (a, b) {
@@ -51,25 +44,11 @@ export default {
   },
   computed: {
     articles(){
-      let posts = this.$site.pages.filter(item => {
+      let feeds = this.$site.pages.filter(item => {
         return item.id == 'feed'
       })
-      this.sortPostsByDate(posts)
-      
-      // insert the year item into the posts array.
-      let lastPostYear;
-      for (let i = 0; i < posts.length; i++) {
-        if(!posts[i].frontmatter) continue;
-
-        const postYear = this.dateFormat(posts[i].frontmatter.date, 'year');
-        if(postYear!=lastPostYear){
-          posts.splice(i, 0, {title: postYear, frontmatter:{date:new Date(postYear+"-01-01").toString()}})
-        }
-
-        lastPostYear = postYear
-      }
-      
-      return posts
+      this.sortPostsByDate(feeds)
+      return feeds
     }
   },
   mounted(){
